bcc06812a0e9c0266c2a06e5276590b29770fff1
jihoon <wlgnstls0413@naver.com>
2025-10-31 14:49:57 +0900

    test(Validator): 테스트 코드 수정 및 추가

--- src/test/java/domain/ValidatorTest.java
+++ src/test/java/domain/ValidatorTest.java
@@ -17,16 +17,21 @@ class ValidatorTest {
         String input = "12000원";
         IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
                 () -> validator.validateNumber(input));
-        assertEquals("[ERROR] 숫자만 입력 가능합니다.", ex.getMessage());
+        assertEquals("[ERROR] 숫자 이외(공백, 문자 등)의 입력을 할 수 없습니다.", ex.getMessage());
     }
 
     @Test
-    @DisplayName("천원 단위의 입력 값이 아닌 경우 에러 발생")
+    @DisplayName("0 이상의 천원 단위의 입력 값이 아닌 경우 에러 발생")
     public void givenNotThousandUnitWhenValidateThenException() {
-        int price = 113002;
-        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
-                () -> validator.validateThousandUnit(price));
-        assertEquals("[ERROR] 천원 단위로만 입력 가능합니다.", ex.getMessage());
+        int price1 = 113002;
+        int price2 = 0;
+
+        IllegalArgumentException ex1 = assertThrows(IllegalArgumentException.class,
+                () -> validator.validateThousandUnit(price1));
+        IllegalArgumentException ex2 = assertThrows(IllegalArgumentException.class,
+                () -> validator.validateThousandUnit(price2));
+        assertEquals("[ERROR] 0이상의 천원 단위로만 입력 가능합니다.", ex1.getMessage());
+        assertEquals("[ERROR] 0이상의 천원 단위로만 입력 가능합니다.", ex2.getMessage());
     }
 
     @Test
@@ -44,14 +49,28 @@ class ValidatorTest {
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("보너스 번호가 당첨 번호와 중복 되었을 떄 에러 발생")
     public void givenBonusNumContainWinningNumbersWhenValidateBonusNumberThenException() {
         List<Integer> winningNumbers = List.of(1,2,3,4,5,6);
-        int bonosNum = 3;
+        String bonus = "3";
 
         IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
-                () -> validator.validateBonusNumber(winningNumbers, bonosNum));
+                () -> validator.validateBonusNumber(winningNumbers, bonus));
         assertEquals("[ERROR] 보너스 번호는 당첨 번호 6자리 숫자와 중복될 수 없습니다.", ex.getMessage());
+    }
+
+    @Test
+    @DisplayName("잘못된 형식의 당첨 번호 입력 시 에러 발생")
+    public void givenWrongWinningNumbersStingInputWhenValidateThenException() {
+        String str1 = "1;2;3;4;5;6";
+        String str2 = "1,2,3,4,5,6,";
+
+        IllegalArgumentException ex1 = assertThrows(IllegalArgumentException.class,
+                () -> validator.validateWinningNumbersStringInput(str1));
+        IllegalArgumentException ex2 = assertThrows(IllegalArgumentException.class,
+                () -> validator.validateWinningNumbersStringInput(str2));
+        assertEquals("[ERROR] 구분자를 확인해 주세요.", ex1.getMessage());
+        assertEquals("[ERROR] 구분자의 형식을 확인해 주세요.", ex2.getMessage());
 
     }
 }
