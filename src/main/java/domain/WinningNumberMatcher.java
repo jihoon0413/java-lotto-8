136d510171b579b4e7e74c6dc7cd7d038897c11f
jihoon <wlgnstls0413@naver.com>
2025-11-02 15:55:00 +0900

    refactor(WinningNumberMatcher): 메소드 입력 타입 변경
    메소드에서 타입을 변환하지 않고 자신의 역할만 할 수 있도록 변경

--- src/main/java/domain/WinningNumberMatcher.java
+++ src/main/java/domain/WinningNumberMatcher.java
@@ -5,6 +5,7 @@ import dto.WinningNumbersResponse;
 import dto.WinningRecordDto;
 import java.util.HashMap;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 import lotto.Lotto;
 
@@ -13,21 +14,19 @@ public class WinningNumberMatcher {
     public WinningRecordDto matchMyLotto(WinningNumbersResponse winningInfo, List<Lotto> myLotto) {
         Map<Rank, Integer> winningRecord = new HashMap<>();
         for (Lotto lotto : myLotto) {
-            int matchCount = countMatchNum(lotto.getNumbers(), winningInfo);
+            int matchCount = countMatchNum(lotto.getNumbers(), winningInfo.getWinningNumbers());
             boolean isMatchBonus = false;
-            if(matchCount == 5 && checkMatchBonus(lotto, winningInfo.getBonusNumber())) {
+            if(matchCount == 5 && checkMatchBonus(lotto.getNumbers(), winningInfo.getBonusNumber())) {
                 isMatchBonus = true;
             }
             Rank myRank = Rank.getRank(matchCount, isMatchBonus);
-
             int count = winningRecord.getOrDefault(myRank,0);
             winningRecord.put(myRank, count+1);
         }
         return WinningRecordDto.of(winningRecord);
     }
 
-    public int countMatchNum(List<Integer> myLotto, WinningNumbersResponse winningInfo) {
-        List<Integer> winningNumbers = winningInfo.getWinningNumbers();
+    public int countMatchNum(List<Integer> myLotto, List<Integer> winningNumbers) {
         int matchCount = 0;
         for (Integer num : myLotto) {
             if(winningNumbers.contains(num)) {
@@ -37,8 +36,8 @@ public class WinningNumberMatcher {
         return matchCount;
     }
 
-    public boolean checkMatchBonus(Lotto myLotto, int bonusNum) {
-        return myLotto.getNumbers().contains(bonusNum);
+    public boolean checkMatchBonus(List<Integer> myLotto, int bonusNum) {
+        return myLotto.contains(bonusNum);
     }
 
 }
